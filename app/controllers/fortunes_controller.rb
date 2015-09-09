aa756bae54df54b47952a9db97882c05d2faa925
Felipe Gentil <cdigentil@gmail.com>
2015-09-09 16:47:32 -0300

    Add database route in fortunes controller

--- app/controllers/fortunes_controller.rb
+++ app/controllers/fortunes_controller.rb
@@ -11,5 +11,13 @@ module Sinatra
       status status_code
     end
 
+    get '/database' do
+      fortune = Fortune.new.find_by(database: params[:q])
+      status_code = fortune.present? ? 200 : 422
+
+      body fortune
+      status status_code
+    end
+
   end
 end
--- spec/controllers/fortunes_controller_spec.rb
+++ spec/controllers/fortunes_controller_spec.rb
@@ -33,9 +33,43 @@ describe Sinatra::FortunesController do
         expect(last_response.body).to be_empty
       end
 
-      it 'returns 200' do
+      it 'returns 422' do
         expect(last_response.status).to eq 422
       end
     end
   end
+
+  describe 'GET /database' do
+    subject { '/database' }
+
+    context 'success' do
+      before { get subject, { q: 'computers' } }
+
+      it 'returns a string' do
+        expect(last_response.body).to be_instance_of(String)
+        expect(last_response.body).not_to be_empty
+      end
+
+      it 'returns 200' do
+        expect(last_response.status).to eq 200
+      end
+    end
+
+    context 'not success' do
+      before do
+        allow_any_instance_of(Fortune).to receive(:find_by) { '' }
+        get subject
+      end
+
+      it 'returns a string' do
+        expect(last_response.body).to be_instance_of(String)
+        expect(last_response.body).to be_empty
+      end
+
+      it 'returns 422' do
+        expect(last_response.status).to eq 422
+      end
+    end
+  end
+
 end
